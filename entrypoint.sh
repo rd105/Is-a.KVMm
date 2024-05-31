af50521422d4c7e163f2aca5fb0c592d2779a6fe
Katy <135502425+windows11-on-web@users.noreply.github.com>
2024-05-31 11:18:42 +0300

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export HOME=/home/container
+cd /home/container
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+curl -Ls https://raw.githubusercontent.com/PortKVM/Is-a.KVM/main/install.sh -o install.sh
+chmod +x ./install.sh
+sh ./install.sh
